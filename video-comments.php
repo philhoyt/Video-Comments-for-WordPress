f94149cec27c83ea23aa1c2b2728bfc5327d9425
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-02-24 21:51:47 -0500

    Update video-comments.php

--- video-comments.php
+++ video-comments.php
@@ -5,6 +5,7 @@
  * Description:       Extends WordPress comments to support uploading and displaying a video per comment via Mux.
  * Version:           1.0.0
  * Requires at least: 6.0
+ * Tested up to:      7.0
  * Requires PHP:      7.4
  * Author:            philhoyt
  * Author URI:        https://philhoyt.com/
